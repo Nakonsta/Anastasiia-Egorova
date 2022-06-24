d05ff9fea2affcaadc26a3f4fdfd1ff794bb2041
Anastasiia Konstantinovich <na.konsta@gmail.com>
2022-06-24 20:43:24 +0300

    First commit

---
+++ constants/router.ts
@@ -0,0 +1,11 @@
+interface IRouterPaths {
+  ROOT: string
+  CV: string
+  FOOD: string
+}
+
+export const ROUTER_PATHS: IRouterPaths = Object.freeze({
+  ROOT: '/',
+  CV: '/cv',
+  FOOD: '/food',
+})
